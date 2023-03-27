727fc5365acdf2e29f9aab165518d50b3ddf09c7
billy <aphexlog@gmail.com>
2023-03-27 17:38:10 -0500

    adding ssm param & env var for OAUTH_TOKEN

--- toby/handler.py
+++ toby/handler.py
@@ -1,9 +1,10 @@
+import os
 import asyncio
 from discord_bot import Bot
 
 # We don't return because otherwise the bot will die.
 def toby_handler(event, context):
-  print("We are going to say hi to toby :wave:")
+	print("We are going to say hi to toby :wave:")
 
-  toby = Bot('<DISCORD TOKEN HERE>')
-  asyncio.run(toby.start())
+	toby = Bot(os.environ['OAUTH_TOKEN'])
+	asyncio.run(toby.start())
